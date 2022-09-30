5b1d83f7ca3c110ea76dc0430cc264aaca9d0828
fragoba <franklin.gomez@o365.unab.edu.co>
2022-09-30 15:21:56 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+function limpiar(){
+    document.getElementById('miCalculadora').reset();
+    document.getElementById('resultado').innerHTML= "";
+}
+
+function sumar(){
+    let numero1 = parseInt(document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let suma = numero1 + numero2;
+    document.getElementById('resultado').innerHTML = suma;
+}
+
+function restar(){
+    let numero1 = parseInt(document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let resta = numero1 - numero2;
+    document.getElementById('resultado').innerHTML = resta;
+}
+
+function multiplicar(){
+    let numero1 = parseInt(document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let multiplicacion = numero1 * numero2;
+    document.getElementById('resultado').innerHTML = multiplicacion;
+}
+
+function dividir(){
+    let numero1 = parseInt(document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let division = numero1 / numero2;
+    document.getElementById('resultado').innerHTML = division;
+}
