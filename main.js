75cbbd1fc81e6d5f1e27d4813948122ceaf5035d
fragoba <franklin.gomez@o365.unab.edu.co>
2022-09-30 18:30:10 -0500

    Ejercicio03

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function area(){
-    let lado1 = document.getElementById('lado1').value;
-    let lado2 = document.getElementById('lado2').value;
-    area = lado1*lado2;
-    document.getElementById('resultado').innerHTML=area;
-}  
+function media(){
+    let numero1 = parseInt(document.getElementById('numero1').value);
+    let numero2 = parseInt(document.getElementById('numero2').value);
+    let numero3 = parseInt(document.getElementById('numero3').value);
+    let numero4 = parseInt(document.getElementById('numero4').value);
+    media = (numero1+numero2+numero3+numero4)/4;
+    document.getElementById('resultado').innerHTML=media;
+} 
+
+function limpiar(){
+    document.getElementById('miCalculadora').reset();
+    document.getElementById('resultado').innerHTML="";
+}
