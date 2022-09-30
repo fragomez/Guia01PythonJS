05d11a908582d86170cb10b1146143ea0cbcf951
fragoba <franklin.gomez@o365.unab.edu.co>
2022-09-30 18:15:07 -0500

    Ejercicio02

--- main.js
+++ main.js
@@ -1,36 +1,6 @@
-/* Función flecha
-let limpiar = () => document.getElementById('miCalculadora').reset(); document.getElementById('resultado').innerHTML="";
-return limpiar; */
-
-function limpiar(){
-    document.getElementById('miCalculadora').reset();
-    document.getElementById('resultado').innerHTML= "";
-}
-
-function sumar(){
-    let numero1 = parseInt(document.getElementById('numero1').value);
-    let numero2 = parseInt(document.getElementById('numero2').value);
-    let suma = numero1 + numero2;
-    document.getElementById('resultado').innerHTML = suma;
-}
-
-function restar(){
-    let numero1 = parseInt(document.getElementById('numero1').value);
-    let numero2 = parseInt(document.getElementById('numero2').value);
-    let resta = numero1 - numero2;
-    document.getElementById('resultado').innerHTML = resta;
-}
-
-function multiplicar(){
-    let numero1 = parseInt(document.getElementById('numero1').value);
-    let numero2 = parseInt(document.getElementById('numero2').value);
-    let multiplicacion = numero1 * numero2;
-    document.getElementById('resultado').innerHTML = multiplicacion;
-}
-
-function dividir(){
-    let numero1 = parseInt(document.getElementById('numero1').value);
-    let numero2 = parseInt(document.getElementById('numero2').value);
-    let division = numero1 / numero2;
-    document.getElementById('resultado').innerHTML = division;
-}
+function area(){
+    let lado1 = document.getElementById('lado1').value;
+    let lado2 = document.getElementById('lado2').value;
+    area = lado1*lado2;
+    document.getElementById('resultado').innerHTML=area;
+}  
