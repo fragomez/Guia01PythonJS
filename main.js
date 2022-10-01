6e101a4bcbc55e95bb010c6052955108d269768a
fragoba <franklin.gomez@o365.unab.edu.co>
2022-09-30 19:48:45 -0500

    Ejercicio16

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 function calcular(){
-    const descuento = 0.10;
-    let costo = parseInt(document.getElementById('costo').value);
-    let subtotal = costo*descuento;
-    let total = costo - subtotal;
+    const porc55 = 0.55;
+    const porc30 = 0.30;
+    const porc15 = 0.15;
+    let nota1 = parseFloat(document.getElementById('nota1').value);
+    let nota2 = parseFloat(document.getElementById('nota2').value);
+    let nota3 = parseFloat(document.getElementById('nota3').value);
+    let exmfin = parseFloat(document.getElementById('exmfin').value);
+    let trbfin = parseFloat(document.getElementById('trbfin').value);
+    let prom = ((nota1+nota2+nota3)/3) * porc55;
+    let promExamFin = exmfin * porc30;
+    let promTrbFin = trbfin * porc15;
+    let notaFinal = prom + promExamFin + promTrbFin; 
     
-    document.getElementById('resultado').innerHTML="$"+total+" pesos";
+    document.getElementById('resultado').innerHTML=notaFinal;
 } 
 
 function limpiar(){
