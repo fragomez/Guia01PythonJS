86ec4e84319e5428cfe0213cbdb700cc0b9f20d9
fragoba <franklin.gomez@o365.unab.edu.co>
2022-09-30 19:54:29 -0500

    Ejercicio18

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 function calcular(){
-    const porc55 = 0.55;
-    const porc30 = 0.30;
-    const porc15 = 0.15;
-    let nota1 = parseFloat(document.getElementById('nota1').value);
-    let nota2 = parseFloat(document.getElementById('nota2').value);
-    let nota3 = parseFloat(document.getElementById('nota3').value);
-    let exmfin = parseFloat(document.getElementById('exmfin').value);
-    let trbfin = parseFloat(document.getElementById('trbfin').value);
-    let prom = ((nota1+nota2+nota3)/3) * porc55;
-    let promExamFin = exmfin * porc30;
-    let promTrbFin = trbfin * porc15;
-    let notaFinal = prom + promExamFin + promTrbFin; 
+    const pagoHora = 15300;
     
-    document.getElementById('resultado').innerHTML=notaFinal;
+    let nombre = document.getElementById('nombre').value;
+    let horas = parseInt(document.getElementById('horas').value);
+    let total = horas * pagoHora; 
+    
+    document.getElementById('resultado').innerHTML="El sueldo total de "+nombre+" es de $" + total + " pesos";
 } 
 
 function limpiar(){
