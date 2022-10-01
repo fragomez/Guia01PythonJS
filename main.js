30058578c634383b437c9311980580c1b238bad1
fragoba <franklin.gomez@o365.unab.edu.co>
2022-09-30 19:33:05 -0500

    Ejercicio13

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-function llamadas(){
-    const minuto = 355;
-    const iva = 0.20;
-    let cantidad = parseInt(document.getElementById('cantidad').value);
-    let minutos = cantidad * minuto;
-    let subtotal = minutos * iva;
-    let total = minutos + subtotal;
+function calcular(){
+    let velocidad = parseInt(document.getElementById('velocidad').value);
+    let velmts = velocidad / 3.6;
     
-    document.getElementById('resultado').innerHTML="$"+total+" pesos";
+    document.getElementById('resultado').innerHTML=velmts+" m/s";
 } 
 
 function limpiar(){
